72d13e5e1ad7c3be76043342238f1b743c43cb1d
delacruzeduardo1998 <delacruzeduard98@gmail.com>
2020-10-10 00:14:08 -0500

    deploy prod

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+'/dist/test-angular'));
+app.get('/*',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/test-angular/index.html'));
+});
+
+app.listen(process.env.PORT || 1234);
+
